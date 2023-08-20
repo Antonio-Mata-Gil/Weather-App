8119fabbe10dd90e56cc19cdb1f57b5a351b06c3
Antonio-Mata-Gil <anto.mata1@gmail.com>
2023-08-20 12:49:31 +0200

    solucion de errores y favicon

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -13,6 +13,6 @@ export const ApiHeader = {
     },
   };
 export const API = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://my-json-server.typicode.com/Antonio-Mata-Gil/Weather-Project/db",
     headers: ApiHeader,
 })
